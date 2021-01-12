8e2a839315bd3bec147df3dedfebae6340054f3a
Ruben Mavarez <rmavarez@glamit.com.ar>
2021-01-12 10:27:47 -0300

    Modificando la promise para que devuelva el result

--- clase-2/desafio-2/tsMavarezRuben.ts
+++ clase-2/desafio-2/tsMavarezRuben.ts
@@ -1,23 +1,23 @@
 import Numeros from './numeros'
 const ops = './operation.ts';
 
-const operation = async (num1: number, num2: number, op: string): Promise<void> => {
+const operation = async (num1: number, num2: number, op: string): Promise<number | undefined> => {
     try {
         const module = await import(ops);
         const result = await module.default.operation(num1, num2, op);
-        console.log(result);
+        return result
     } catch (error) {
         console.log(error)
     }
 }
 
-const operations = (func: any): Promise<void> => {
+const operations = (func: Promise<number | undefined>) => {
     return func
 }
 
 const numeros1 = new Numeros(1, 2);
 const numeros2 = new Numeros(5, 1);
 
-operations(operation(numeros1.getNumeros()[0], numeros1.getNumeros()[1], 'suma'));
-operations(operation(numeros2.getNumeros()[0], numeros2.getNumeros()[1], 'resta'));
+operations(operation(numeros1.getNumeros()[0], numeros1.getNumeros()[1], 'suma')).then(res => console.log(res));
+operations(operation(numeros2.getNumeros()[0], numeros2.getNumeros()[1], 'resta')).then(res => console.log(res));
 
