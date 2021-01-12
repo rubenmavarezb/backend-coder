76d05d20d507b312e221df86959be4a669aa564c
Ruben Mavarez <rmavarez@glamit.com.ar>
2021-01-11 21:04:30 -0300

    test

---
+++ clase-2/index.ts
@@ -0,0 +1,52 @@
+class User {
+    name: string;
+    lastname: string;
+    book: any;
+    pet: string[]
+
+    constructor(name: string, lastname: string, book:[], pet:[]) {
+        this.name = name;
+        this.lastname = lastname;
+        this.book = book;
+        this.pet = pet;
+    }
+    getFullName() {
+        return `Your name is ${this.name} and last name ${this.lastname}`;
+    }
+    addPet(mascot: string) {
+        this.pet = [...this.pet, mascot];
+    }
+    getPet() {
+        return `You have ${this.pet.length} pets`;
+    }
+    addBook(author: string, book: string) {
+        this.book = [...this.book, { author: author, book: book }];
+    }
+    getBook() {
+        const bookTitles = this.book.map((title: any) => title.book);
+        return bookTitles;
+    }
+}
+
+const books = [
+    {
+        author: "John Katzenback",
+        book: "The Psicoanalist"
+    },
+    {
+        author: "Gabriel García Márquez",
+        book: "100 años de soledad"
+    }
+]
+
+const pets = ['Farru the dog', 'Misu the cat']
+
+const user = new User('Ruben', 'Mavarez', books, pets)
+
+console.log(user);
+
+console.log(user.getFullName());
+user.addPet('Mickey the mouse');
+console.log(user.getPet());
+user.addBook('Shakespeare', 'Romeo & Juliet');
+console.log(user.getBook());
