14950cb9c7ccb9f2715846dce9de279b50f1a87e
Ruben Mavarez <rmavarez@glamit.com.ar>
2021-02-17 21:04:44 -0300

    arreglando problema con hbs

--- clase-10/index.js
+++ clase-10/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_handlebars_1 = __importDefault(require("express-handlebars"));
 var express_1 = __importDefault(require("express"));
+var uuid_1 = require("uuid");
 var app = express_1.default();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
@@ -12,17 +13,20 @@ var products = [
     {
         id: 1,
         producto: 'televisor',
-        precio: 1000
+        precio: 1000,
+        thumbnail: 'image.jpg'
     },
     {
         id: 2,
         producto: 'telefono',
-        precio: 3000
+        precio: 3000,
+        thumbnail: 'image2.jpg'
     },
     {
         id: 3,
         producto: 'heladera',
-        precio: 5000
+        precio: 5000,
+        thumbnail: 'image3.jpg'
     }
 ];
 app.engine('hbs', express_handlebars_1.default({
@@ -31,11 +35,25 @@ app.engine('hbs', express_handlebars_1.default({
     layoutsDir: __dirname + '/views/layouts',
     partialsDir: __dirname + '/views/partials'
 }));
-app.set("view engine", "hbs");
-app.set("views", "./views");
+app.set("view engine", ".hbs");
+app.set("views", __dirname + "/views");
 app.get('/', function (req, res) {
     console.log(__dirname);
     res.render('main', { products: products, listExists: true });
+});
+app.get('/add', function (req, res) {
+    res.sendFile('C:/Users/Glamit/Desktop/backend-coder/clase-10/public/index.html');
+});
+app.post('/formsuccess', function (req, res) {
+    var _a = req.body, producto = _a.producto, precio = _a.precio, thumbnail = _a.thumbnail;
+    var newProduct = {
+        id: uuid_1.v4(),
+        producto: producto,
+        precio: precio,
+        thumbnail: thumbnail
+    };
+    products.push(newProduct);
+    res.send("\n        <h4>Producto agregado</h4>\n        <a href=\"http://localhost:8080\">Ir a productos</a>\n    ");
 });
 app.listen(8080, function () {
     console.log('Servidor corriendo en el puerto 8080');
--- clase-10/index.ts
+++ clase-10/index.ts
@@ -1,5 +1,6 @@
 import Handlebars from 'express-handlebars';
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = express();
 
@@ -7,26 +8,30 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
 interface Product {
-    id: number;
+    id: number | string;
     producto: string;
     precio: number;
+    thumbnail: string;
 }
 
 const products: Product[] = [
     {
         id: 1,
         producto: 'televisor',
-        precio: 1000
+        precio: 1000,
+        thumbnail: 'image.jpg'
     },
     {
         id: 2,
         producto: 'telefono',
-        precio: 3000
+        precio: 3000,
+        thumbnail: 'image2.jpg'
     },
     {
         id: 3,
         producto: 'heladera',
-        precio: 5000
+        precio: 5000,
+        thumbnail: 'image3.jpg'
     }
 ]
 
@@ -40,13 +45,35 @@ app.engine(
     })
 )
 
-app.set("view engine", "hbs");
-app.set("views", "./views");
+app.set("view engine", ".hbs");
+app.set("views", __dirname + "/views");
 
 app.get('/', (req, res) => {
     console.log(__dirname)
     res.render('main', {products: products, listExists: true})
 })
+
+app.get('/add', (req, res) => {
+    res.sendFile('C:/Users/Glamit/Desktop/backend-coder/clase-10/public/index.html')
+});
+
+app.post('/formsuccess', (req, res) => {
+    const { producto, precio, thumbnail } = req.body;
+
+    const newProduct: Product = {
+        id: uuidv4(),
+        producto,
+        precio,
+        thumbnail
+    }
+    
+    products.push(newProduct);
+    
+    res.send(`
+        <h4>Producto agregado</h4>
+        <a href="http://localhost:8080">Ir a productos</a>
+    `)
+});
 
 app.listen(8080, () => {
     console.log('Servidor corriendo en el puerto 8080');
